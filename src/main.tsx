8dab43e1902618f9e96b5c5ce287a50aebde5361
rahulthampi <trahulthampi@gmail.com>
2023-11-04 16:13:18 +0530

    chore: initialize '@radix-ui/themes'

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Theme } from '@radix-ui/themes';
+
 import App from './App.tsx';
+
+import '@radix-ui/themes/styles.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </React.StrictMode>,
 );
