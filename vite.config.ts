adb55a8af4108bc861c9af046b74073d3e3f1a10
rahulthampi <trahulthampi@gmail.com>
2023-11-04 21:45:47 +0530

    chore: path alias prefix change from '@/*' to '@@/*'

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// // https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@/UIKit': path.resolve(__dirname, './src/UIKit'),
+      '@@': path.resolve(__dirname, './src'),
+      '@@/UIKit': path.resolve(__dirname, './src/UIKit'),
     },
   },
 });
