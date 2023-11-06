3d809c2541ff0df4669b8b5c9d2bc268dacd5fd2
rahulthampi <trahulthampi@gmail.com>
2023-11-06 13:45:21 +0530

    chore(setup): jest and react-testing-library

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:storybook/recommended',
+    'react-app/jest',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
